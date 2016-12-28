6e05915e5bca35e2e70f705a0dda6c063cde6b61
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2016-12-28 21:18:38 -0200

    First commit

---
+++ src/main/java/com/codeonblue/SocialNetworkOneApplication.java
@@ -0,0 +1,12 @@
+package com.codeonblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkOneApplication.class, args);
+	}
+}
